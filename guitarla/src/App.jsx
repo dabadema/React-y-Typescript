1f3638cff6f0bfa32606817cc3c14a8184cdef08
dabadema <dabadema7@hotmail.com>
2024-06-10 22:31:36 +0200

    Showing each guitar info

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -16,8 +16,8 @@ function App() {
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
-                    {data.map(() => (
-                        <Guitar price={100} auth={true} />
+                    {data.map((guitar) => (
+                        <Guitar key={guitar.id} guitar={guitar} />
                     ))}
                 </div>
             </main>
