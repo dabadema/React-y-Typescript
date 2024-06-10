83fd213fa2f47e9167a3d701b3410d6656c0ea71
dabadema <dabadema7@hotmail.com>
2024-06-10 22:17:03 +0200

    Props in Guitar component

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
                 <div className="row mt-5">
                     {data.map(() => (
-                        <Guitar />
+                        <Guitar price={100} auth={true} />
                     ))}
                 </div>
             </main>
