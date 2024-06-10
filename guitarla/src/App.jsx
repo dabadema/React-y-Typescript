bd7a3c429f108f98270710716dd75245c03c2644
dabadema <dabadema7@hotmail.com>
2024-06-10 23:07:44 +0200

    Creating and Cart State

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -5,6 +5,11 @@ import { db } from './data/db';
 
 function App() {
     const [data, setData] = useState(db);
+    const [cart, setCart] = useState([]);
+
+    function addToCart(item) {
+        setCart((prevCart) => [...prevCart, item]);
+    }
 
     return (
         <>
@@ -15,7 +20,12 @@ function App() {
 
                 <div className="row mt-5">
                     {data.map((guitar) => (
-                        <Guitar key={guitar.id} guitar={guitar} />
+                        <Guitar
+                            key={guitar.id}
+                            guitar={guitar}
+                            setCart={setCart}
+                            addToCart={addToCart}
+                        />
                     ))}
                 </div>
             </main>
