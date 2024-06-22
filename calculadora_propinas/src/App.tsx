c0daad60751f76f8b74ee56e2d5a25375e26541b
dabadema <dabadema7@hotmail.com>
2024-06-22 17:32:16 +0200

    Feat: Header created

--- calculadora_propinas/src/App.tsx
+++ calculadora_propinas/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
     return (
         <>
-            <h1 className="text-8xl font-black"> Hola Mundo</h1>
+            <header className="bg-teal-400 py-5">
+                <h1 className="text-center text-4xl font-black">
+                    Calculadora de Propinas y consumos
+                </h1>
+            </header>
         </>
     );
 }
