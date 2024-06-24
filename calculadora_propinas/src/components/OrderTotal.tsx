f15ba79e44e70c4fb34008a2c0af19cbc983a24c
dabadema <dabadema7@hotmail.com>
2024-06-24 20:02:42 +0200

    Feat: Creating OrderTotal with subtotal, tips and total

---
+++ calculadora_propinas/src/components/OrderTotal.tsx
@@ -0,0 +1,21 @@
+export default function OrderTotal() {
+    return (
+        <>
+            <div className="space-y-3 ">
+                <h2 className="font-black text-2xl">Totales y propinas</h2>
+                <p>
+                    Subtotal a pagar {''}
+                    <span className="font-bold"> $0</span>
+                </p>
+                <p>
+                    Propina {''}
+                    <span className="font-bold"> $0 </span>
+                </p>
+                <p>
+                    Total a pagar {''}
+                    <span className="font-bold"> $0 </span>
+                </p>
+            </div>
+        </>
+    );
+}
