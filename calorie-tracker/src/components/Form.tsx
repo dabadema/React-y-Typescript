971cd83ae2df7606c5a77d4a4b1ddd25cb56374f
dabadema <dabadema7@hotmail.com>
2024-06-26 21:55:35 +0200

    Using the state with handleChange function

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -8,6 +8,13 @@ export default function Form() {
         calories: 0,
     });
 
+    const handleChange = (e) => {
+        setActivity({
+            ...activity,
+            [e.target.id]: e.target.value,
+        });
+    };
+
     return (
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className="grid grid-cols-1 gap-3">
@@ -18,9 +25,11 @@ export default function Form() {
                 <select
                     className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                     id="category"
+                    value={activity.category}
+                    onChange={handleChange}
                 >
                     {categories.map((category) => (
-                        <option key={category.id} value={activity.category}>
+                        <option key={category.id} value={category.id}>
                             {category.name}
                         </option>
                     ))}
@@ -38,6 +47,7 @@ export default function Form() {
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
                     value={activity.name}
+                    onChange={handleChange}
                 />
             </div>
 
@@ -52,6 +62,7 @@ export default function Form() {
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Calorias. Ej. 300 o 500"
                     value={activity.calories}
+                    onChange={handleChange}
                 />
             </div>
 
