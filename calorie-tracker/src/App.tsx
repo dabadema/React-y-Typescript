a28fde4f1ebe846aa687a46984d4d8a28c920f64
dabadema <dabadema7@hotmail.com>
2024-07-04 18:53:37 +0200

    Feat: Showing calories consumed

--- calorie-tracker/src/App.tsx
+++ calorie-tracker/src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo } from 'react';
 import { activityReducer, initialState } from './reducers/activity-reducer';
 import Form from './components/Form';
 import ActivityList from './components/ActivityList';
+import CalorieTracker from './components/CalorieTracker';
 
 function App() {
     const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -34,6 +35,13 @@ function App() {
                     <Form dispatch={dispatch} state={state} />
                 </div>
             </section>
+
+            <section className="bg-gray-800 py-10">
+                <div className="max-w-4xl mx-auto">
+                    <CalorieTracker activities={state.activities} />
+                </div>
+            </section>
+
             <section className="p-10 mx-auto max-w-4xl">
                 <ActivityList activities={state.activities} dispatch={dispatch} />
             </section>
