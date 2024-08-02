909f565fd949efb3c0f6b45ff8b78e00e0139931
dabadema <dabadema7@hotmail.com>
2024-08-02 18:03:06 +0200

    Feat: ErrorMessage component shown when form is not filled

--- control-gastos-contextapi/src/components/ExpenseForm.tsx
+++ control-gastos-contextapi/src/components/ExpenseForm.tsx
@@ -4,6 +4,7 @@ import 'react-calendar/dist/Calendar.css';
 import 'react-date-picker/dist/DatePicker.css';
 import { ChangeEvent, useState } from 'react';
 import { DraftExpense, Value } from '../types';
+import ErrorMessage from './ErrorMessage';
 
 export default function ExpenseForm() {
     const [expense, setExpense] = useState<DraftExpense>({
@@ -12,6 +13,8 @@ export default function ExpenseForm() {
         category: '',
         date: new Date(),
     });
+
+    const [error, setError] = useState('');
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
         const { name, value } = e.target;
@@ -29,11 +32,23 @@ export default function ExpenseForm() {
         });
     };
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (Object.values(expense).includes('')) {
+            setError('Todos los campos son obligatorios');
+            return;
+        }
+        console.log('todo bien');
+    };
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
                 Nuevo Gasto
             </legend>
+
+            {error && <ErrorMessage> {error} </ErrorMessage>}
 
             <div className="flex flex-col gap-2">
                 <label htmlFor="expenseName" className="text-xl">
