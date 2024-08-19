2270aedc1d435dc9015fe784e1c0b43b83794d47
dabadema <dabadema7@hotmail.com>
2024-08-19 22:54:38 +0200

    Feat: importing context to Form component and building the app

--- calorie-tracker-contextapi-usereducer/src/components/Form.tsx
+++ calorie-tracker-contextapi-usereducer/src/components/Form.tsx
@@ -1,14 +1,9 @@
-import { useState, ChangeEvent, Dispatch, useEffect } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import type { Activity } from '../types';
 import { categories } from '../data/categories';
-import { ActivityActions, ActivityState } from '../reducers/activity-reducer';
-
-type FormProps = {
-    dispatch: Dispatch<ActivityActions>;
-    state: ActivityState;
-};
+import { useActivity } from '../hooks/useActivity';
 
 const initialState: Activity = {
     id: uuidv4(),
@@ -17,7 +12,8 @@ const initialState: Activity = {
     calories: 0,
 };
 
-export default function Form({ dispatch, state }: FormProps) {
+export default function Form() {
+    const { state, dispatch } = useActivity();
     const [activity, setActivity] = useState<Activity>(initialState);
 
     useEffect(() => {
