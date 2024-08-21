824157ceb9655616602779eef9bdd7387a5bc0af
dabadema <dabadema7@hotmail.com>
2024-08-21 17:02:08 +0200

    Feat: First error messages shown after submitting

--- pacientes-zustand/src/components/PatientForm.tsx
+++ pacientes-zustand/src/components/PatientForm.tsx
@@ -1,7 +1,15 @@
 import { useForm } from 'react-hook-form';
 
 export default function PatientForm() {
-    const { register } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const registerPatient = () => {
+        console.log('Nuevo Paciente');
+    };
 
     return (
         <div className="md:w-1/2 lg:w-2/5 mx-5">
@@ -12,7 +20,11 @@ export default function PatientForm() {
                 <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
 
-            <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" noValidate>
+            <form
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+                noValidate
+                onSubmit={handleSubmit(registerPatient)}
+            >
                 <div className="mb-5">
                     <label htmlFor="name" className="text-sm uppercase font-bold">
                         Paciente
@@ -26,6 +38,7 @@ export default function PatientForm() {
                             required: 'El nombre del paciente es obligatorio',
                         })}
                     />
+                    {errors.name?.message}
                 </div>
 
                 <div className="mb-5">
