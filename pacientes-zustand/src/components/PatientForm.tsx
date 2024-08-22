26c925044c47ab8c67547137914f21aed0af1b86
dabadema <dabadema7@hotmail.com>
2024-08-22 23:35:02 +0200

    Feat: Defining types to the PatientForm

--- pacientes-zustand/src/components/PatientForm.tsx
+++ pacientes-zustand/src/components/PatientForm.tsx
@@ -1,15 +1,16 @@
 import { useForm } from 'react-hook-form';
 import Error from './Error';
+import type { DraftPatient } from '../types';
 
 export default function PatientForm() {
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm();
+    } = useForm<DraftPatient>();
 
-    const registerPatient = () => {
-        console.log('Nuevo Paciente');
+    const registerPatient = (data: DraftPatient) => {
+        console.log(data);
     };
 
     console.log(errors);
@@ -42,7 +43,7 @@ export default function PatientForm() {
                         })}
                     />
 
-                    {errors.name && <Error> {errors.name?.message?.toString()} </Error>}
+                    {errors.name && <Error> {errors.name?.message} </Error>}
                 </div>
 
                 <div className="mb-5">
@@ -58,7 +59,7 @@ export default function PatientForm() {
                             required: 'El propietario es obligatorio',
                         })}
                     />
-                    {errors.caretaker && <Error> {errors.caretaker?.message?.toString()} </Error>}
+                    {errors.caretaker && <Error> {errors.caretaker?.message} </Error>}
                 </div>
 
                 <div className="mb-5">
@@ -78,7 +79,7 @@ export default function PatientForm() {
                             },
                         })}
                     />
-                    {errors.email && <Error> {errors.email?.message?.toString()} </Error>}
+                    {errors.email && <Error> {errors.email?.message} </Error>}
                 </div>
 
                 <div className="mb-5">
@@ -93,7 +94,7 @@ export default function PatientForm() {
                             required: 'La fecha de alta es obligatoria',
                         })}
                     />
-                    {errors.date && <Error> {errors.date?.message?.toString()} </Error>}
+                    {errors.date && <Error> {errors.date?.message} </Error>}
                 </div>
 
                 <div className="mb-5">
@@ -108,7 +109,7 @@ export default function PatientForm() {
                             required: 'Los sintomas son obligatorios',
                         })}
                     />
-                    {errors.symptoms && <Error> {errors.symptoms?.message?.toString()} </Error>}
+                    {errors.symptoms && <Error> {errors.symptoms?.message} </Error>}
                 </div>
 
                 <input
