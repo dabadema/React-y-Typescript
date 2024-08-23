c5d2247f18d0ffcbaa0e19e870d07cb27d7658a2
dabadema <dabadema7@hotmail.com>
2024-08-23 11:41:59 +0200

    Feat: install uuid and typing in Zustand State

--- pacientes-zustand/src/components/PatientList.tsx
+++ pacientes-zustand/src/components/PatientList.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { usePatientStore } from './store';
 
 export default function PatientList() {
+    const patients = usePatientStore((state) => state.patients);
+
+    console.log(patients);
+
     return <div>PatientList</div>;
 }
