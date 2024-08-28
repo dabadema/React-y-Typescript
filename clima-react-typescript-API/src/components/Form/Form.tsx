9448b9869fb65f89f7bfce1627a799b1b7867f5e
dabadema <dabadema7@hotmail.com>
2024-08-28 22:07:26 +0200

    Feat: Creating forms for checking weather

---
+++ clima-react-typescript-API/src/components/Form/Form.tsx
@@ -0,0 +1,24 @@
+import { countries } from '../../data/countries';
+
+export default function Form() {
+    return (
+        <form>
+            <div>
+                <label htmlFor="city"> Ciudad:</label>
+                <input id="city" type="text" name="city" placeholder="Ciudad" />
+            </div>
+            <div>
+                <label htmlFor="country"> Pais:</label>
+                <select>
+                    <option value="">--- Seleccione un Pais ---</option>
+                    {countries.map((country) => (
+                        <option key={country.code} value={country.code}>
+                            {country.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <input type="submit" value="Consultar Clima" />
+        </form>
+    );
+}
