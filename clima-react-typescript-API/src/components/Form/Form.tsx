2e1feb49349ff10225259bc04dd10f004844b7fc
dabadema <dabadema7@hotmail.com>
2024-09-02 23:23:55 +0200

    Feat: Creating custom Hook for checking the weather to the API

--- clima-react-typescript-API/src/components/Form/Form.tsx
+++ clima-react-typescript-API/src/components/Form/Form.tsx
@@ -4,7 +4,11 @@ import { SearchType } from '../../types';
 import styles from './Form.module.css';
 import Alert from '../Alert/Alert';
 
-export default function Form() {
+type FromProps = {
+    fetchWeather: () => void;
+};
+
+export default function Form({ fetchWeather }: FromProps) {
     const [search, setSearch] = useState<SearchType>({
         city: '',
         country: '',
@@ -27,6 +31,7 @@ export default function Form() {
             setAlert('Todos los campos son obligatorios');
             return;
         }
+        fetchWeather();
     };
 
     return (
