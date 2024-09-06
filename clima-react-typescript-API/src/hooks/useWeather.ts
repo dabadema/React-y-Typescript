80da931f52b49547382aa65c16a96c9804b253d7
dabadema <dabadema7@hotmail.com>
2024-09-06 20:34:58 +0200

    Feat: Typing results from API with Type Guards or Assertions

--- clima-react-typescript-API/src/hooks/useWeather.ts
+++ clima-react-typescript-API/src/hooks/useWeather.ts
@@ -1,6 +1,18 @@
 import axios from 'axios';
 import { SearchType, Weather } from '../types';
 
+// Type Guard or Assertion
+function isWeatherResponse(weather: unknown) {
+    return (
+        Boolean(weather) &&
+        typeof weather === 'object' &&
+        typeof (weather as Weather).name === 'string' &&
+        typeof (weather as Weather).main.temp === 'number' &&
+        typeof (weather as Weather).main.temp_max === 'number' &&
+        typeof (weather as Weather).main.temp_min === 'number'
+    );
+}
+
 export default function useWeather() {
     const fetchWeather = async (search: SearchType) => {
         const appId = import.meta.env.VITE_API_KEY;
@@ -15,9 +27,17 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
 
+            //Castear el type from index.ts file
+            // const { data: weatherResult } = await axios<Weather>(weatherUrl);
+            // console.log(weatherResult.main.temp);
+            // console.log(weatherResult.name);
+
+            //Type Guards or Assertion
             const { data: weatherResult } = await axios<Weather>(weatherUrl);
-            console.log(weatherResult.main.temp);
-            console.log(weatherResult.name);
+            const result = isWeatherResponse(weatherResult);
+            if (result) {
+                console.log(weatherResult.main);
+            }
         } catch (error) {
             console.log(error);
         }
