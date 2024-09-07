4ae68733d6da14b138da5787f55606b1105a27a9
dabadema <dabadema7@hotmail.com>
2024-09-07 10:44:25 +0200

    Feat: Typing response from API with ZOD

--- clima-react-typescript-API/src/hooks/useWeather.ts
+++ clima-react-typescript-API/src/hooks/useWeather.ts
@@ -1,17 +1,30 @@
 import axios from 'axios';
+import { z } from 'zod';
 import { SearchType, Weather } from '../types';
 
 // Type Guard or Assertion
-function isWeatherResponse(weather: unknown) {
-    return (
-        Boolean(weather) &&
-        typeof weather === 'object' &&
-        typeof (weather as Weather).name === 'string' &&
-        typeof (weather as Weather).main.temp === 'number' &&
-        typeof (weather as Weather).main.temp_max === 'number' &&
-        typeof (weather as Weather).main.temp_min === 'number'
-    );
-}
+// function isWeatherResponse(weather: unknown) {
+//     return (
+//         Boolean(weather) &&
+//         typeof weather === 'object' &&
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' &&
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number'
+//     );
+// }
+
+// ZOD
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number(),
+    }),
+});
+
+type Weather = z.infer<typeof Weather>;
 
 export default function useWeather() {
     const fetchWeather = async (search: SearchType) => {
@@ -33,10 +46,20 @@ export default function useWeather() {
             // console.log(weatherResult.name);
 
             //Type Guards or Assertion
-            const { data: weatherResult } = await axios<Weather>(weatherUrl);
-            const result = isWeatherResponse(weatherResult);
-            if (result) {
-                console.log(weatherResult.main);
+            // const { data: weatherResult } = await axios<Weather>(weatherUrl);
+            // const result = isWeatherResponse(weatherResult);
+            // if (result) {
+            //     console.log(weatherResult.main);
+            // }
+
+            // ZOD
+            const { data: weatherResult } = await axios(weatherUrl);
+            const result = Weather.safeParse(weatherResult);
+            if (result.success) {
+                console.log(result.data.name);
+                console.log(result.data.main.temp);
+            } else {
+                console.log('Respuesta mal formada....'); //En el caso que el z.object no esté bien formado va a entrar aqui.
             }
         } catch (error) {
             console.log(error);
