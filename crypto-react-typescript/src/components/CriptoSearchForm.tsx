acd5115515f2dd9984fbafb0edcec8be3103fa29
dabadema <dabadema7@hotmail.com>
2024-09-14 17:13:36 +0200

    Feat: Creating Pair State for currency and cryptocurrency

--- crypto-react-typescript/src/components/CriptoSearchForm.tsx
+++ crypto-react-typescript/src/components/CriptoSearchForm.tsx
@@ -1,15 +1,28 @@
+import { ChangeEvent, useState } from 'react';
 import { currencies } from '../data';
 import { useCryptoStore } from '../store';
+import { Pair } from '../types';
 
 export default function CriptoSearchForm() {
     const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
+    const [pair, setPair] = useState<Pair>({
+        currency: '',
+        cryptocurrency: '',
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setPair({
+            ...pair,
+            [e.target.name]: e.target.value,
+        });
+    };
 
     return (
         <>
             <form className="form">
                 <div className="field">
                     <label htmlFor="currency"> Moneda</label>
-                    <select name="currency" id="currency">
+                    <select name="currency" id="currency" onChange={handleChange}>
                         <option value="">-- Seleccione --</option>
                         {currencies.map((currency) => (
                             <option key={currency.code} value={currency.code}>
@@ -19,8 +32,8 @@ export default function CriptoSearchForm() {
                     </select>
                 </div>
                 <div className="field">
-                    <label htmlFor="criptocurrency"> Criptomoneda</label>
-                    <select name="criptocurrency" id="criptocurrency">
+                    <label htmlFor="cryptocurrency"> Criptomoneda</label>
+                    <select name="cryptocurrency" id="cryptocurrency" onChange={handleChange}>
                         <option value="">-- Seleccione --</option>
                         {cryptocurrencies.map((crypto) => (
                             <option key={crypto.CoinInfo.FullName} value={crypto.CoinInfo.Name}>
