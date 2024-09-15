3e7add0fded0790972b11f8a1d70fa833346c3d9
dabadema <dabadema7@hotmail.com>
2024-09-15 09:25:06 +0200

    Feat: CSS styles added and params added

--- crypto-react-typescript/src/components/CriptoPriceDisplay.tsx
+++ crypto-react-typescript/src/components/CriptoPriceDisplay.tsx
@@ -6,14 +6,30 @@ export default function CriptoPriceDisplay() {
     const hasResult = useMemo(() => !Object.values(result).includes(''), [result]);
 
     return (
-        <div>
+        <div className="result-wrapper">
             {hasResult && (
                 <>
                     <h2>Cotización</h2>
                     <div className="result">
+                        <img
+                            src={`https://cryptocompare.com/${result.IMAGEURL}`}
+                            alt="Imagen Crypto"
+                        />
                         <div>
                             <p>
                                 El precio es de: <span>{result.PRICE}</span>
+                            </p>
+                            <p>
+                                Precio más alto del día: <span>{result.HIGHDAY}</span>
+                            </p>
+                            <p>
+                                Precio más bajo del día: <span>{result.LOWDAY}</span>
+                            </p>
+                            <p>
+                                Variación últimas 24h: <span>{result.CHANGEPCT24HOUR}</span>
+                            </p>
+                            <p>
+                                Última actualización: <span>{result.LASTUPDATE}</span>
                             </p>
                         </div>
                     </div>
