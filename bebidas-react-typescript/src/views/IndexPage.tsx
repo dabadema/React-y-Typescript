69ecd9f24b48dad8fc601accdec5a53e9d9012e3
dabadema <dabadema7@hotmail.com>
2024-10-12 13:00:56 +0200

    Feat: Header component created

--- bebidas-react-typescript/src/views/IndexPage.tsx
+++ bebidas-react-typescript/src/views/IndexPage.tsx
@@ -1,3 +1,9 @@
+import Header from '../components/Header';
+
 export default function IndexPage() {
-    return <div>IndexPage</div>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
