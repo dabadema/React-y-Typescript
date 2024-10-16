41e3d4e756917f13021a23aa0f29d21500d3bb50
dabadema <dabadema7@hotmail.com>
2024-10-16 23:12:29 +0200

    Feat: Searching recipes by category and ingredient and validating the response from API

--- bebidas-react-typescript/src/types/index.ts
+++ bebidas-react-typescript/src/types/index.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod';
-import { CategoriesAPIResponseSchema, SearchFiltersSchema } from '../utils/recipes-schema';
+import {
+    CategoriesAPIResponseSchema,
+    DrinksAPIResponse,
+    SearchFiltersSchema,
+} from '../utils/recipes-schema';
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilters = z.infer<typeof SearchFiltersSchema>;
+export type Drinks = z.infer<typeof DrinksAPIResponse>;
