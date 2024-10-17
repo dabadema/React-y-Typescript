abcefd6a7e71a75b5b283c936184a46ae784441c
dabadema <dabadema7@hotmail.com>
2024-10-17 19:44:30 +0200

    Feat: Showing the cards with the recipes

--- bebidas-react-typescript/src/views/IndexPage.tsx
+++ bebidas-react-typescript/src/views/IndexPage.tsx
@@ -11,7 +11,7 @@ export default function IndexPage() {
         <>
             <h1 className="text-6xl font-extrabold">Inicio</h1>
             {hasDrinks ? (
-                <div>
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-10">
                     {drinks.drinks.map((drink) => (
                         <DrinkCard key={drink.idDrink} drink={drink} />
                     ))}
