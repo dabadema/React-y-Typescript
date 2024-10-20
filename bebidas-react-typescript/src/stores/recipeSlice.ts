570f1166b0c9fd0ca327672677f6035ed8cbbb4c
dabadema <dabadema7@hotmail.com>
2024-10-20 23:41:38 +0200

    Feat: Adding HeadlessUI to get a modal for showing the recipes

--- bebidas-react-typescript/src/stores/recipeSlice.ts
+++ bebidas-react-typescript/src/stores/recipeSlice.ts
@@ -7,9 +7,11 @@ export type RecipesSliceType = {
     categories: Categories;
     drinks: Drinks;
     selectedRecipe: Recipe;
+    modal: boolean;
     fetchCategories: () => Promise<void>;
     searchRecipes: (searchFilters: SearchFilters) => Promise<void>;
     selectRecipe: (id: Drink['idDrink']) => Promise<void>;
+    closeModal: () => void;
 };
 
 export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
@@ -20,6 +22,7 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
         drinks: [],
     },
     selectedRecipe: {} as Recipe,
+    modal: false,
     fetchCategories: async () => {
         const categories = await getCategories();
         set({
@@ -37,6 +40,13 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
         const selectedRecipe = await getRecipeById(id);
         set({
             selectedRecipe,
+            modal: true,
+        });
+    },
+    closeModal: () => {
+        set({
+            modal: false,
+            selectedRecipe: {} as Recipe,
         });
     },
 });
