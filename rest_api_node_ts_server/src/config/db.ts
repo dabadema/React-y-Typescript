49ec6e51ba163ca94f38ac6e1bf3aeff9601f129
dabadema <dabadema7@hotmail.com>
2024-10-26 00:22:37 +0200

    Installing "dotenv"

--- rest_api_node_ts_server/src/config/db.ts
+++ rest_api_node_ts_server/src/config/db.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
 const db = new Sequelize(
     'postgresql://neondb_owner:D7SG6MZKcYjL@ep-morning-sky-a2nheoeb.eu-central-1.aws.neon.tech/neondb?sslmode=require'
