a576afb96e74d913a1d6c124f8ec7f95ac83946f
dabadema <dabadema7@hotmail.com>
2024-10-26 00:21:17 +0200

    Feat: Connecting the RestAPI to Neon database

---
+++ rest_api_node_ts_server/src/config/db.ts
@@ -0,0 +1,7 @@
+import { Sequelize } from 'sequelize';
+
+const db = new Sequelize(
+    'postgresql://neondb_owner:D7SG6MZKcYjL@ep-morning-sky-a2nheoeb.eu-central-1.aws.neon.tech/neondb?sslmode=require'
+);
+
+export default db;
--- rest_api_node_ts_server/src/server.ts
+++ rest_api_node_ts_server/src/server.ts
@@ -1,5 +1,19 @@
 import express from 'express';
 import router from './router';
+import db from './config/db';
+
+//Connect to DB
+async function connectDB() {
+    try {
+        await db.authenticate();
+        console.log('Connected to DB successfully');
+        db.sync();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB();
 
 const server = express();
 
