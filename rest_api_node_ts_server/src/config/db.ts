996fcb43c820586a6f1ed91442dc2121ef31865d
dabadema <dabadema7@hotmail.com>
2024-10-26 00:26:48 +0200

    Feat: creating connection to db with env variables

--- rest_api_node_ts_server/src/config/db.ts
+++ rest_api_node_ts_server/src/config/db.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-const db = new Sequelize(
-    'postgresql://neondb_owner:D7SG6MZKcYjL@ep-morning-sky-a2nheoeb.eu-central-1.aws.neon.tech/neondb?sslmode=require'
-);
+dotenv.config();
+
+const db = new Sequelize(process.env.DATABASE_URL!);
 
 export default db;
