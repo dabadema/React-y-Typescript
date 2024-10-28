053a369502daddd47c16bd8b9f8ac9774a9443fd
dabadema <dabadema7@hotmail.com>
2024-10-28 23:31:41 +0100

    Feat: Validating how to create Products in the handler

--- rest_api_node_ts_server/src/handlers/product.ts
+++ rest_api_node_ts_server/src/handlers/product.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from 'express';
+import { check, validationResult } from 'express-validator';
 import Product from '../models/Product.model';
 
 export const createProduct = async (req: Request, res: Response) => {
+    //Validating the response
+    await check('name').notEmpty().withMessage('Name is required').run(req);
+    await check('price')
+        .isNumeric()
+        .withMessage('Not valid value')
+        .notEmpty()
+        .withMessage('Price is required')
+        .custom((value) => value > 0)
+        .withMessage('Price must be greater than 0')
+        .run(req);
+
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     const product = await Product.create(req.body);
 
     res.json({ data: product, message: 'Product created successfully' });
