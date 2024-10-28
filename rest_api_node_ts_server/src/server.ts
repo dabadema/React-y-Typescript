76d4a7977c747ec037f4eb92483263647215b34d
dabadema <dabadema7@hotmail.com>
2024-10-28 16:03:18 +0100

    Feat: Colors installed and used in log messages

--- rest_api_node_ts_server/src/server.ts
+++ rest_api_node_ts_server/src/server.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import router from './router';
 import db from './config/db';
+import colors from 'colors';
 
 //Connect to DB
 async function connectDB() {
     try {
         await db.authenticate();
-        console.log('Connected to DB successfully');
+        console.log(colors.green('Connected to DB successfully'));
         db.sync();
     } catch (error) {
         console.log(error);
+        console.log(colors.red('Failed to connect to DB'));
     }
 }
 
