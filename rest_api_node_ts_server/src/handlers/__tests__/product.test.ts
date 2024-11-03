dbc2b967be587e5c52cb2f94c4768103c0cc2a5f
dabadema <dabadema7@hotmail.com>
2024-11-04 00:17:25 +0100

    Feat: Creating testing for GET product by ID

--- rest_api_node_ts_server/src/handlers/__tests__/product.test.ts
+++ rest_api_node_ts_server/src/handlers/__tests__/product.test.ts
@@ -52,19 +52,26 @@ describe('POST /api/products', () => {
     });
 });
 
-describe('GET /api/products', () => {
-    it('Should check whether api/products url exists', async () => {
-        const response = await request(server).get('/api/products');
-        expect(response.status).toBe(200);
+describe('GET /api/products/:id', () => {
+    it('Should return a 404 response if the product is not found', async () => {
+        const productId = '20000    ';
+        const response = await request(server).get(`/api/products/${productId}`);
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body.message).toBe('Product not found');
     });
 
-    it('Should get a JSON with all products', async () => {
-        const response = await request(server).get('/api/products');
-        expect(response.status).toBe(200);
-        expect(response.headers['content-type']).toContain('application/json');
-        expect(response.body.data).toHaveLength(1);
+    it('Should check a valid ID in the URL', async () => {
+        const response = await request(server).get('/api/products/not-valid-url');
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe('Id must be a number');
+    });
 
-        expect(response.body).not.toHaveProperty('errors');
-        expect(response.status).not.toBe(404);
+    it('get a JSON response for a single product', async () => {
+        const response = await request(server).get('/api/products/1');
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('data');
     });
 });
--- rest_api_node_ts_server/src/handlers/product.ts
+++ rest_api_node_ts_server/src/handlers/product.ts
@@ -14,13 +14,16 @@ export const getProducts = async (req: Request, res: Response) => {
     }
 };
 
-export const getProductById = async (req: Request, res: Response) => {
+export const getProductById = async (req: Request, res: Response): Promise<void> => {
     try {
         const { id } = req.params;
         const product = await Product.findByPk(id);
+
         if (!product) {
             res.status(404).json({ message: 'Product not found' });
+            return;
         }
+
         res.json({ data: product });
     } catch (error) {
         console.log(error);
@@ -28,12 +31,18 @@ export const getProductById = async (req: Request, res: Response) => {
     }
 };
 
-export const createProduct = async (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response): Promise<void> => {
     try {
         const product = await Product.create(req.body);
-        res.status(201).json({ data: product, message: 'Product created successfully' });
+        res.status(201).json({
+            data: product,
+            message: 'Product created successfully',
+        });
     } catch (error) {
-        res.status(500).json({ message: 'Internal server error', error });
+        res.status(500).json({
+            message: 'Internal server error',
+            error,
+        });
     }
 };
 
