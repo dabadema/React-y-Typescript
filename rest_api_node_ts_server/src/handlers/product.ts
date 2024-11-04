3657948f130c50033f6cd1e9fbece00ecaa37ff6
dabadema <dabadema7@hotmail.com>
2024-11-04 13:23:29 +0100

    Refactoring: Eliminating unnecessary code

--- rest_api_node_ts_server/src/handlers/product.ts
+++ rest_api_node_ts_server/src/handlers/product.ts
@@ -2,48 +2,31 @@ import { Request, Response } from 'express';
 import Product from '../models/Product.model';
 
 export const getProducts = async (req: Request, res: Response) => {
-    try {
-        const products = await Product.findAll({
-            order: [['price', 'ASC']],
-            attributes: { exclude: ['availability', 'createdAt', 'updatedAt'] },
-        });
-        res.json({ data: products });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
+    const products = await Product.findAll({
+        order: [['price', 'ASC']],
+        attributes: { exclude: ['availability', 'createdAt', 'updatedAt'] },
+    });
+    res.json({ data: products });
 };
 
 export const getProductById = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
 
-        if (!product) {
-            res.status(404).json({ message: 'Product not found' });
-            return;
-        }
-
-        res.json({ data: product });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Internal server error' });
+    if (!product) {
+        res.status(404).json({ message: 'Product not found' });
+        return;
     }
+
+    res.json({ data: product });
 };
 
 export const createProduct = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json({
-            data: product,
-            message: 'Product created successfully',
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error',
-            error,
-        });
-    }
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        data: product,
+        message: 'Product created successfully',
+    });
 };
 
 export const updateProduct = async (req: Request, res: Response): Promise<void> => {
