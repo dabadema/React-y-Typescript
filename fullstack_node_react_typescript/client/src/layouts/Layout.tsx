d44a1e6b27a66a99b596e18cc697b701185fc1c9
dabadema <dabadema7@hotmail.com>
2024-11-19 23:43:54 +0100

    Feat: Working with the layout

--- fullstack_node_react_typescript/client/src/layouts/Layout.tsx
+++ fullstack_node_react_typescript/client/src/layouts/Layout.tsx
@@ -3,8 +3,17 @@ import { Outlet } from 'react-router-dom';
 export default function Layout() {
     return (
         <div>
-            <h1>Layout</h1>
-            <Outlet />
+            <header className="bg-slate-800 p-4">
+                <div className="mx-auto max-w-6xl py-10">
+                    <h1 className="text-4xl font-extrabold text-white">
+                        Administrador de Productos
+                    </h1>
+                </div>
+            </header>
+
+            <main className="mx-auto max-w-6xl p-10 bg-white rounded-md shadow-md">
+                <Outlet />
+            </main>
         </div>
     );
 }
