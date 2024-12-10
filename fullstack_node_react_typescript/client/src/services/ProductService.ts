4fa6619f4a5c57bc0e344993290a26a1fdf73462
dabadema <dabadema7@hotmail.com>
2024-12-10 23:38:35 +0100

    Feat: Validating data with ZOD before sending to the API

--- fullstack_node_react_typescript/client/src/services/ProductService.ts
+++ fullstack_node_react_typescript/client/src/services/ProductService.ts
@@ -1,7 +1,21 @@
+import { safeParse } from 'valibot';
+import { DraftProductSchema } from '../types/Index';
+
 type ProductData = {
     [k: string]: FormDataEntryValue;
 };
 
 export async function addProduct(data: ProductData) {
-    console.log(data);
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price,
+        });
+        if (result.success) {
+        } else {
+            throw new Error('Datos no validos');
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
