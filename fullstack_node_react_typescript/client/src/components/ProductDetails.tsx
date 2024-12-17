34d6fd6c5c7d41f5911d22631cb11266ef61d67e
dabadema <dabadema7@hotmail.com>
2024-12-17 12:43:43 +0100

    Feat: Adding button to edit any product

--- fullstack_node_react_typescript/client/src/components/ProductDetails.tsx
+++ fullstack_node_react_typescript/client/src/components/ProductDetails.tsx
@@ -1,4 +1,5 @@
 import { Product } from '../types';
+import { Link, useNavigate } from 'react-router-dom';
 import { formatCurrency } from '../utils';
 
 type ProductDetailsProps = {
@@ -17,7 +18,12 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
             </td>
             <td className="p-3 text-lg text-gray-800 ">
                 <div className="flex gap-2 items-center">
-                    <button>Editar</button>
+                    <Link
+                        to={`productos/${product.id}/editar`}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase text-xs text-center font-bold"
+                    >
+                        Editar
+                    </Link>
                 </div>
             </td>
         </tr>
