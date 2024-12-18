8890ca73c5d6c7521cbb2aca513ad74f0c16f6a7
dabadema <dabadema7@hotmail.com>
2024-12-18 12:11:02 +0100

    Feat: Updating the products

--- fullstack_node_react_typescript/client/src/services/ProductService.ts
+++ fullstack_node_react_typescript/client/src/services/ProductService.ts
@@ -69,7 +69,10 @@ export async function updateProduct(data: ProductData, id: Product['id']) {
             availability: toBoolean(data.availability.toString()),
         });
 
-        console.log(result);
+        if (result.success) {
+            const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+            await axios.put(url, result.output);
+        }
     } catch (error) {
         console.log(error);
     }
