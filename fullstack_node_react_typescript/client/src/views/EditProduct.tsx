dfa795552ab62dadc8b3cf6a98b5909246fff8bb
dabadema <dabadema7@hotmail.com>
2024-12-18 11:41:00 +0100

    Feat: Adding the availlability while editing the product

--- fullstack_node_react_typescript/client/src/views/EditProduct.tsx
+++ fullstack_node_react_typescript/client/src/views/EditProduct.tsx
@@ -39,6 +39,11 @@ export async function action({ request, params }: ActionFunctionArgs) {
     return redirect('/');
 }
 
+const availabilityOptions = [
+    { name: 'Disponible', value: true },
+    { name: 'No Disponible', value: false },
+];
+
 export default function EditProduct() {
     const product = useLoaderData() as Product;
     const error = useActionData() as string;
@@ -84,6 +89,25 @@ export default function EditProduct() {
                         defaultValue={product.price}
                     />
                 </div>
+
+                <div className="mb-4">
+                    <label className="text-gray-800" htmlFor="availability">
+                        Disponibilidad:
+                    </label>
+                    <select
+                        id="availability"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        name="availability"
+                        defaultValue={product?.availability.toString()}
+                    >
+                        {availabilityOptions.map((option) => (
+                            <option key={option.name} value={option.value.toString()}>
+                                {option.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
                 <input
                     type="submit"
                     className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
