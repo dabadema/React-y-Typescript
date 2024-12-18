598f71afbd4ea7de0184df297778020750b7860c
dabadema <dabadema7@hotmail.com>
2024-12-18 12:38:00 +0100

    Feat: Creating an reusable component

--- fullstack_node_react_typescript/client/src/views/EditProduct.tsx
+++ fullstack_node_react_typescript/client/src/views/EditProduct.tsx
@@ -8,8 +8,9 @@ import {
     useLoaderData,
 } from 'react-router-dom';
 import ErrorMessage from '../components/ErrorMessage';
-import { addProduct, getProductbyId, updateProduct } from '../services/ProductService';
+import { getProductbyId, updateProduct } from '../services/ProductService';
 import { Product } from '../types';
+import ProductForm from '../components/ProductForm';
 
 export async function loader({ params }: LoaderFunctionArgs) {
     if (params.id !== undefined) {
@@ -63,32 +64,7 @@ export default function EditProduct() {
             {error && <ErrorMessage> {error}</ErrorMessage>}
 
             <Form className="mt-10" method="post">
-                <div className="mb-4">
-                    <label className="text-gray-800" htmlFor="name">
-                        Nombre Producto:
-                    </label>
-                    <input
-                        id="name"
-                        type="text"
-                        className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Nombre del Producto"
-                        name="name"
-                        defaultValue={product.name}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="text-gray-800" htmlFor="price">
-                        Precio:
-                    </label>
-                    <input
-                        id="price"
-                        type="number"
-                        className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Precio Producto. ej. 200, 300"
-                        name="price"
-                        defaultValue={product.price}
-                    />
-                </div>
+                <ProductForm product={product} />
 
                 <div className="mb-4">
                     <label className="text-gray-800" htmlFor="availability">
@@ -111,7 +87,7 @@ export default function EditProduct() {
                 <input
                     type="submit"
                     className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-                    value="Registrar Producto"
+                    value="Guardar Cambios"
                 />
             </Form>
         </>
