f96811ddc5d5346696e941412c9987f210e8d8c0
dabadema <dabadema7@hotmail.com>
2024-12-21 09:01:50 +0100

    Feat: model Project type with ProjectType

--- Uptask/backend_uptask/src/models/Project.ts
+++ Uptask/backend_uptask/src/models/Project.ts
@@ -24,6 +24,6 @@ const ProjectSchema: Schema = new Schema({
     },
 });
 
-const Project = mongoose.model('Project', ProjectSchema);
+const Project = mongoose.model<ProjectType>('Project', ProjectSchema);
 
 export default Project;
