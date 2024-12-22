14c26ac6154d499d4d07c21116e1997268d59bdf
dabadema <dabadema7@hotmail.com>
2024-12-22 18:52:59 +0100

    Feat: Creating route for getAllProjects

--- Uptask/backend_uptask/src/server.ts
+++ Uptask/backend_uptask/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv, { config } from 'dotenv';
 import { connectDB } from './config/db';
+import projectRoutes from './routes/projectRoutes';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+// Routes
+
+app.use('/api/projects', projectRoutes);
 
 export default app;
