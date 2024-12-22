65b4ff228d208af06930a60baae952c63d72e1e6
dabadema <dabadema7@hotmail.com>
2024-12-22 19:08:41 +0100

    Feat: Creating first projects into our db

--- Uptask/backend_uptask/src/controllers/ProjectControllers.ts
+++ Uptask/backend_uptask/src/controllers/ProjectControllers.ts
@@ -1,12 +1,19 @@
 import type { Request, Response } from 'express';
+import Project from '../models/Project';
 
 export class ProjectController {
     static createProject = async (req: Request, res: Response) => {
-        console.log(req.body);
-        res.send('Creando proyecto...');
+        const project = new Project(req.body);
+
+        try {
+            await project.save();
+            res.send('Project created properly');
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     static getAllProjects = async (req: Request, res: Response) => {
-        res.send('Todos los proyectos');
+        res.send('Every project');
     };
 }
