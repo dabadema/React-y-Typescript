573bbee51384638fbecf7eb161dda888609490f0
dabadema <dabadema7@hotmail.com>
2024-12-22 19:25:41 +0100

    Feat: Adding handleInputErrors middleware for Body´s validation

--- Uptask/backend_uptask/src/routes/projectRoutes.ts
+++ Uptask/backend_uptask/src/routes/projectRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectControllers';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
-router.post('/', ProjectController.createProject);
+router.post(
+    '/',
+    body('projectName').notEmpty().withMessage('Project´s name is mandatory'),
+    body('clientName').notEmpty().withMessage('Client´s name is mandatory'),
+    body('description').notEmpty().withMessage('Project´s description is mandatory'),
+    handleInputErrors,
+    ProjectController.createProject
+);
 router.get('/', ProjectController.getAllProjects);
 
 export default router;
