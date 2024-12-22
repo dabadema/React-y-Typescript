d2210667489fd8c0b0a46deac1e7fe09b7334ced
dabadema <dabadema7@hotmail.com>
2024-12-22 19:00:39 +0100

    Creating route for creatingProjects

--- Uptask/backend_uptask/src/controllers/ProjectControllers.ts
+++ Uptask/backend_uptask/src/controllers/ProjectControllers.ts
@@ -1,6 +1,11 @@
 import type { Request, Response } from 'express';
 
 export class ProjectController {
+    static createProject = async (req: Request, res: Response) => {
+        console.log(req.body);
+        res.send('Creando proyecto...');
+    };
+
     static getAllProjects = async (req: Request, res: Response) => {
         res.send('Todos los proyectos');
     };
--- Uptask/backend_uptask/src/routes/projectRoutes.ts
+++ Uptask/backend_uptask/src/routes/projectRoutes.ts
@@ -3,6 +3,7 @@ import { ProjectController } from '../controllers/ProjectControllers';
 
 const router = Router();
 
+router.post('/', ProjectController.createProject);
 router.get('/', ProjectController.getAllProjects);
 
 export default router;
