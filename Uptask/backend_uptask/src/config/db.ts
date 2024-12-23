e8090f0f6ba5353fb1db2470d84793762bb29acd
dabadema <dabadema7@hotmail.com>
2024-12-23 15:13:56 +0100

    Feat: console.logs translated into spanish

--- Uptask/backend_uptask/src/config/db.ts
+++ Uptask/backend_uptask/src/config/db.ts
@@ -6,9 +6,9 @@ export const connectDB = async () => {
     try {
         const { connection } = await mongoose.connect(process.env.DATABASE_URL);
         const url = `${connection.host}:${connection.port}`;
-        console.log(colors.magenta.bold(`MongoDB Conectado en: ${url}`));
+        console.log(colors.magenta.bold(`MongoDB connected in: ${url}`));
     } catch (error) {
-        console.log(colors.red.bold('Error al conectar a MongoDB'));
+        console.log(colors.red.bold('Error connecting to MongoDB'));
         exit(1);
     }
 };
