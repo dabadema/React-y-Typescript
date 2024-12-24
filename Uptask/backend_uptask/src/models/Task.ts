4dbacf1de8d9aba936694db61b4a152b66d337bf
dabadema <dabadema7@hotmail.com>
2024-12-24 19:27:23 +0100

    Feat: Relating Project & Task models

--- Uptask/backend_uptask/src/models/Task.ts
+++ Uptask/backend_uptask/src/models/Task.ts
@@ -1,22 +1,32 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document, Types } from 'mongoose';
 
 export interface ITask extends Document {
     name: string;
     description: string;
+    project: Types.ObjectId;
 }
 
-export const TaskSchema: Schema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
+export const TaskSchema: Schema = new Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        descrition: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        project: {
+            type: Types.ObjectId,
+            ref: 'Project',
+            trim: true,
+            required: true,
+        },
     },
-    descrition: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 const Task = mongoose.model<ITask>('Task', TaskSchema);
 export default Task;
