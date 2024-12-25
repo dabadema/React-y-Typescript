b97146ca4266f24f47a7b4b082d2dceb40671650
dabadema <dabadema7@hotmail.com>
2024-12-25 20:09:12 +0100

    Feat: First steps into task controller

--- Uptask/backend_uptask/src/routes/projectRoutes.ts
+++ Uptask/backend_uptask/src/routes/projectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectControllers';
 import { handleInputErrors } from '../middleware/validation';
+import { TaskController } from '../controllers/TaskController';
 
 const router = Router();
 
@@ -38,5 +39,8 @@ router.delete(
     handleInputErrors,
     ProjectController.deleteProjectById
 );
+
+/** Routes for tasks */
+router.post('/:projectId/tasks', TaskController.createProject);
 
 export default router;
