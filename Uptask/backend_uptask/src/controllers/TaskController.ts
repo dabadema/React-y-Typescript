d857bcb387a94a856866b868b97d6bd8c17f97de
dabadema <dabadema7@hotmail.com>
2024-12-28 22:36:34 +0100

    Feat: Creating an endpoint for modifying taskStatus

--- Uptask/backend_uptask/src/controllers/TaskController.ts
+++ Uptask/backend_uptask/src/controllers/TaskController.ts
@@ -73,7 +73,7 @@ export class TaskController {
     static deleteTaskById = async (req: Request, res: Response) => {
         try {
             const { taskId } = req.params;
-            const task = await Task.findById(taskId, req.body);
+            const task = await Task.findById(taskId);
 
             if (!task) {
                 const error = new Error('Task not found');
@@ -85,6 +85,26 @@ export class TaskController {
             await Promise.allSettled([task.deleteOne(), req.project.save()]);
 
             res.send('Task deleted properly');
+        } catch (error) {
+            res.status(500).json({ error: 'There was an error' });
+        }
+    };
+
+    static updateTaskStatus = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params;
+            const task = await Task.findById(taskId);
+            if (!task) {
+                const error = new Error('Task not found');
+                res.status(404).json({ error: error.message });
+                return;
+            }
+
+            const { status } = req.body;
+            task.status = status;
+            await task.save();
+
+            res.send('Task status updated properly');
         } catch (error) {
             res.status(500).json({ error: 'There was an error' });
         }
