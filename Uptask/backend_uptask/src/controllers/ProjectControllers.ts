c26c4b3e41b3bdfd3bfece8c4bc8f76640e805b7
dabadema <dabadema7@hotmail.com>
2024-12-29 08:41:53 +0100

    Refactor: Exchanging findByIdAndUpdate for findById

--- Uptask/backend_uptask/src/controllers/ProjectControllers.ts
+++ Uptask/backend_uptask/src/controllers/ProjectControllers.ts
@@ -42,11 +42,14 @@ export class ProjectController {
         const { id } = req.params;
 
         try {
-            const project = await Project.findByIdAndUpdate(id, req.body);
+            const project = await Project.findById(id);
             if (!project) {
                 const error = new Error('Project not found');
                 res.status(404).json({ error: error.message });
             }
+            project.clientName = req.body.clientName;
+            project.projectName = req.body.projectName;
+            project.description = req.body.description;
             await project.save();
             res.send('Project updated');
         } catch (error) {
