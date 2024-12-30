20efb03dab43d2d3fd7fd5b870793928b741e24e
dabadema <dabadema7@hotmail.com>
2024-12-30 18:13:37 +0100

    Feat: Application Layout designed

--- Uptask/uptask_frontend/src/layouts/AppLayout.tsx
+++ Uptask/uptask_frontend/src/layouts/AppLayout.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Logo from '../components/Logo';
 
 export default function AppLayout() {
     return (
         <>
-            <div>AppLayout</div>
+            <header className="bg-gray-800 text-white py-5">
+                <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+                    <div className="w-64">
+                        <Logo />
+                    </div>
+                </div>
+            </header>
 
-            <Outlet />
+            <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+                <Outlet />
+            </section>
+
+            <footer className="py-5">
+                <p className="text-center text-gray-500">
+                    Todos los derechos reservados {new Date().getFullYear()}
+                </p>
+            </footer>
         </>
     );
 }
