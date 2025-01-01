8e08c11d7bfb823d194e4ae70da7c42c7266566a
dabadema <dabadema7@hotmail.com>
2025-01-01 14:16:44 +0100

    Feat: Creating the view to for the dashboard of every project

--- Uptask/uptask_frontend/src/views/DashboardView.tsx
+++ Uptask/uptask_frontend/src/views/DashboardView.tsx
@@ -1,3 +1,20 @@
+import { Link } from 'react-router-dom';
+
 export default function DashboardView() {
-    return <div>Dashboard</div>;
+    return (
+        <>
+            <h1 className="text-5xl font-black">My projects</h1>
+            <p className="text-2xl font-light text-gray-500 mt-5">
+                Manage and track your projects.
+            </p>
+            <nav className="my-5">
+                <Link
+                    className="bg-purple-400 hover:bg-purple-500 transition-colors text-white text-xl font-bold cursor-pointer px-10 py-3 mt-5 "
+                    to="/projects/create"
+                >
+                    New project
+                </Link>
+            </nav>
+        </>
+    );
 }
