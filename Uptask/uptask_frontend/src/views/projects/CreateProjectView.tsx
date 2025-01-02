73a9d2dfb297cca04730c69532f4755b04c42877
dabadema <dabadema7@hotmail.com>
2025-01-02 08:52:09 +0100

    Feat: Navigating to home after creating a project

--- Uptask/uptask_frontend/src/views/projects/CreateProjectView.tsx
+++ Uptask/uptask_frontend/src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import ProjectForm from '@/components/ProjectForm';
 import { ProjectFormData } from 'types';
 import { createProject } from '@/api/ProjectAPI';
 
 export default function DashboardView() {
+    const navigate = useNavigate();
     const initialValues: ProjectFormData = {
         projectName: '',
         clientName: '',
@@ -17,8 +18,9 @@ export default function DashboardView() {
         formState: { errors },
     } = useForm({ defaultValues: initialValues });
 
-    const handleForm = (data: ProjectFormData) => {
-        createProject(data);
+    const handleForm = async (data: ProjectFormData) => {
+        await createProject(data);
+        navigate('/');
     };
 
     return (
