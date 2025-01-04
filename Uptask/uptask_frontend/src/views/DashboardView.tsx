7c836fe3bbddd934335c20cdaf0218101214acf3
dabadema <dabadema7@hotmail.com>
2025-01-04 23:49:04 +0100

    Feat: Using useQuery for retrieving all projects from backend to the dashboard

--- Uptask/uptask_frontend/src/views/DashboardView.tsx
+++ Uptask/uptask_frontend/src/views/DashboardView.tsx
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getProjects } from '@/api/ProjectAPI';
 
 export default function DashboardView() {
+    const { data, isLoading } = useQuery({ queryKey: ['projects'], queryFn: getProjects });
+    if (isLoading) return <div>Loading...</div>;
+    console.log(data);
+
     return (
         <>
             <h1 className="text-5xl font-black">My projects</h1>
