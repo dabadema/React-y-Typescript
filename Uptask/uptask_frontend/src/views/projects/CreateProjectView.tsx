14cdc1bc7f2d0e4b2492467b100aaa5abfc5caa5
dabadema <dabadema7@hotmail.com>
2025-01-04 23:19:16 +0100

    Feat: Creating projects using mutations from react-query

--- Uptask/uptask_frontend/src/views/projects/CreateProjectView.tsx
+++ Uptask/uptask_frontend/src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import ProjectForm from '@/components/ProjectForm';
 import { ProjectFormData } from 'types';
@@ -19,11 +20,16 @@ export default function DashboardView() {
         formState: { errors },
     } = useForm({ defaultValues: initialValues });
 
-    const handleForm = async (formData: ProjectFormData) => {
-        const data = await createProject(formData);
-        toast.success(data);
-        navigate('/');
-    };
+    const { mutate } = useMutation({
+        mutationFn: createProject,
+        onError: () => {},
+        onSuccess: (data) => {
+            toast.success(data);
+            navigate('/');
+        },
+    });
+
+    const handleForm = async (formData: ProjectFormData) => mutate(formData);
 
     return (
         <>
