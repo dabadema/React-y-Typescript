b7020c96b8a1e1617f6a55a1482b9ca596032e68
dabadema <dabadema7@hotmail.com>
2025-01-05 18:20:26 +0100

    Feat: Morgan installed and applied into server

--- Uptask/backend_uptask/src/server.ts
+++ Uptask/backend_uptask/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv, { config } from 'dotenv';
 import cors from 'cors';
+import morgan from 'morgan';
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
@@ -13,10 +14,11 @@ const app = express();
 
 app.use(cors(corsConfig));
 
+// Logging
+app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
-
 app.use('/api/projects', projectRoutes);
 
 export default app;
