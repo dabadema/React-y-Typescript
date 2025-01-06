ad6729f2587bac5b2f94db6ac1fcec94dcfd1e21
dabadema <dabadema7@hotmail.com>
2025-01-06 17:52:59 +0100

    Fix: Fixing the SafeParse for the response getProjectById

--- Uptask/uptask_frontend/src/api/ProjectAPI.ts
+++ Uptask/uptask_frontend/src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from 'axios';
-import { dashboardProjectSchema, Project, ProjectFormData, projectSchema } from '../types';
+import { dashboardProjectSchema, Project, ProjectFormData } from '../types';
 import api from '@/lib/axios';
 
 export async function createProject(formData: ProjectFormData) {
@@ -30,10 +30,7 @@ export async function getProjects() {
 export async function getProjectById(projectId: Project['_id']) {
     try {
         const { data } = await api.get(`/projects/${projectId}`);
-        const response = projectSchema.safeParse(data);
-        if (response.success) {
-            return response.data;
-        }
+        return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
