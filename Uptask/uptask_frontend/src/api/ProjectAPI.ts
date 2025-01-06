c8ebfa0b6b5ab4c223fd2ca1bc9fa7a62af455e9
dabadema <dabadema7@hotmail.com>
2025-01-06 11:50:22 +0100

    Feat: SafeParse applied to getProjectById and ProjectDetailsView component created

--- Uptask/uptask_frontend/src/api/ProjectAPI.ts
+++ Uptask/uptask_frontend/src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from 'axios';
-import { dashboardProjectSchema, Project, ProjectFormData } from '../types';
+import { dashboardProjectSchema, Project, ProjectFormData, projectSchema } from '../types';
 import api from '@/lib/axios';
 
 export async function createProject(formData: ProjectFormData) {
@@ -30,7 +30,10 @@ export async function getProjects() {
 export async function getProjectById(projectId: Project['_id']) {
     try {
         const { data } = await api.get(`/projects/${projectId}`);
-        return data;
+        const response = projectSchema.safeParse(data);
+        if (response.success) {
+            return response.data;
+        }
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
--- Uptask/uptask_frontend/src/views/projects/ProjectDetailsView.tsx
+++ Uptask/uptask_frontend/src/views/projects/ProjectDetailsView.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getProjectById } from '@/api/ProjectAPI';
+import AddTaskModal from '@/components/tasks/AddTaskModal';
 
 export default function ProjectDetailsView() {
-    return <div>Project Details</div>;
+    const navigate = useNavigate();
+
+    const params = useParams();
+    const projectId = params.projectId!;
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['projects'],
+        queryFn: () => getProjectById(projectId),
+        retry: false,
+    });
+
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <Navigate to="/404" />;
+
+    if (data)
+        return (
+            <>
+                <h1 className="text-5xl font-black">{data.projectName}</h1>
+                <p className="text-2xl text-gray-500 font-light mt-5">
+                    Client: <span className="text-gray-600">{data.clientName}</span>
+                </p>
+                <p className="text-2xl text-gray-500 font-light mt-5">
+                    Description: <span className="text-gray-600">{data.description}</span>
+                </p>
+                <nav className="my-5 flex gap-3">
+                    <button
+                        type="button"
+                        className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+                        onClick={() => navigate(`?newTask=true`)}
+                    >
+                        Add Task
+                    </button>
+                </nav>
+
+                <AddTaskModal />
+            </>
+        );
+
+    return <div>Edit Project View</div>;
 }
