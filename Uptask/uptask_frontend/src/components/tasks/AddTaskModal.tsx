85c4a651f32b9c94991ba1d23d143a0448db9523
dabadema <dabadema7@hotmail.com>
2025-01-06 17:05:49 +0100

    Feat: Creating Tasks with useMutation

--- Uptask/uptask_frontend/src/components/tasks/AddTaskModal.tsx
+++ Uptask/uptask_frontend/src/components/tasks/AddTaskModal.tsx
@@ -1,18 +1,25 @@
 import { Fragment } from 'react';
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
 import TaskForm from './TaskForm';
 import { TaskFormData } from '@/types/index';
+import { createTask } from '@/api/TaskAPI';
+import { toast } from 'react-toastify';
 
 export default function AddTaskModal() {
     const navigate = useNavigate();
 
+    /** Reading if modal exists */
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
-
     const modalTask = queryParams.get('newTask');
     const showModal = modalTask ? true : false;
+
+    /** Reading projectId */
+    const params = useParams();
+    const projectId = params.projectId!;
 
     const initialValues: TaskFormData = {
         name: '',
@@ -22,13 +29,30 @@ export default function AddTaskModal() {
     const {
         register,
         handleSubmit,
+        reset,
         formState: { errors },
     } = useForm<TaskFormData>({
         defaultValues: initialValues,
     });
 
+    const { mutate } = useMutation({
+        mutationFn: createTask,
+        onError: (error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            toast.success(data);
+            reset();
+            navigate(`/projects/${projectId}`);
+        },
+    });
+
     const handleCreateTask = (formData: TaskFormData) => {
-        console.log(formData);
+        const data = {
+            formData,
+            projectId,
+        };
+        mutate(data);
     };
 
     return (
