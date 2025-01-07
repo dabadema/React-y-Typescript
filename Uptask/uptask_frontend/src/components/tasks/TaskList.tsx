3e7e576eedd05c083069bb8129085bb2c6122177
dabadema <dabadema7@hotmail.com>
2025-01-07 21:26:51 +0100

    Feat: Applying different colors to each status

--- Uptask/uptask_frontend/src/components/tasks/TaskList.tsx
+++ Uptask/uptask_frontend/src/components/tasks/TaskList.tsx
@@ -17,6 +17,14 @@ const initialStatusGroups: GroupedTasks = {
     completed: [],
 };
 
+const statusColors: { [key: string]: string } = {
+    pending: 'border-t-slate-500',
+    onHold: 'border-t-red-500',
+    inProgress: 'border-t-blue-500',
+    underReview: 'border-t-amber-500',
+    completed: 'border-t-emerald-500',
+};
+
 const statusTranslations: { [key: string]: string } = {
     pending: 'Pendiente',
     onHold: 'En Espera',
@@ -41,7 +49,9 @@ export default function TaskList({ tasks }: TaskListProps) {
             <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
-                        <h3 className="capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8">
+                        <h3
+                            className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusColors[status]}`}
+                        >
                             {statusTranslations[status]}
                         </h3>
 
