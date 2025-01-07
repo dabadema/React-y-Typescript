db67e648731dc590abb251fac74e514647a346b5
dabadema <dabadema7@hotmail.com>
2025-01-07 22:27:16 +0100

    Feat: getTaskById API added to TaskAPI

--- Uptask/uptask_frontend/src/api/TaskAPI.ts
+++ Uptask/uptask_frontend/src/api/TaskAPI.ts
@@ -1,16 +1,29 @@
 import api from '@/lib/axios';
 import { isAxiosError } from 'axios';
-import { Project, TaskFormData } from '@/types/index';
+import { Project, Task, TaskFormData } from '@/types/index';
 
 type taskAPI = {
     formData: TaskFormData;
     projectId: Project['_id'];
+    taskId: Task['_id'];
 };
 
 export async function createTask({ formData, projectId }: Pick<taskAPI, 'formData' | 'projectId'>) {
     try {
         const url = `/projects/${projectId}/tasks`;
         const { data } = await api.post<string>(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function getTaskById({ projectId, taskId }: Pick<taskAPI, 'projectId' | 'taskId'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`;
+        const { data } = await api.get(url);
         return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
