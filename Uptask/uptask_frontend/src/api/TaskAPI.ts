28508ed1f4204ca7f5d415c82a239b20a4c00296
dabadema <dabadema7@hotmail.com>
2025-01-08 18:20:33 +0100

    Feat: Validating response from backend with ZOD

--- Uptask/uptask_frontend/src/api/TaskAPI.ts
+++ Uptask/uptask_frontend/src/api/TaskAPI.ts
@@ -1,6 +1,6 @@
 import api from '@/lib/axios';
 import { isAxiosError } from 'axios';
-import { Project, Task, TaskFormData } from '@/types/index';
+import { Project, Task, TaskFormData, taskSchema } from '@/types/index';
 
 type TaskAPI = {
     formData: TaskFormData;
@@ -24,8 +24,11 @@ export async function createTask({ formData, projectId }: Pick<TaskAPI, 'formDat
 export async function getTaskById({ projectId, taskId }: Pick<TaskAPI, 'projectId' | 'taskId'>) {
     try {
         const url = `/projects/${projectId}/tasks/${taskId}`;
-        const { data } = await api.get(url);
-        return data;
+        const { data } = await api.get<Task>(url);
+        const response = taskSchema.safeParse(data);
+        if (response.success) {
+            return response.data;
+        }
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
--- Uptask/uptask_frontend/src/components/tasks/TaskModalDetails.tsx
+++ Uptask/uptask_frontend/src/components/tasks/TaskModalDetails.tsx
@@ -30,58 +30,61 @@ export default function TaskModalDetails() {
         return <Navigate to={`/projects/${projectId}`} />;
     }
 
-    return (
-        <>
-            <Transition appear show={showModal} as={Fragment}>
-                <Dialog
-                    as="div"
-                    className="relative z-10"
-                    onClose={() => {
-                        navigate(location.pathname, { replace: true });
-                    }}
-                >
-                    <TransitionChild
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
+    if (data)
+        return (
+            <>
+                <Transition appear show={showModal} as={Fragment}>
+                    <Dialog
+                        as="div"
+                        className="relative z-10"
+                        onClose={() => {
+                            navigate(location.pathname, { replace: true });
+                        }}
                     >
-                        <div className="fixed inset-0 bg-black/60" />
-                    </TransitionChild>
+                        <TransitionChild
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="fixed inset-0 bg-black/60" />
+                        </TransitionChild>
 
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <TransitionChild
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                                    <p className="text-sm text-slate-400">Creation date: </p>
-                                    <p className="text-sm text-slate-400">Last update: </p>
-                                    <DialogTitle
-                                        as="h3"
-                                        className="font-black text-4xl text-slate-600 my-5"
-                                    >
-                                        Title here
-                                    </DialogTitle>
-                                    <p className="text-lg text-slate-500 mb-2">Description:</p>
-                                    <div className="my-5 space-y-3">
-                                        <label className="font-bold">Actual status:</label>
-                                    </div>
-                                </DialogPanel>
-                            </TransitionChild>
+                        <div className="fixed inset-0 overflow-y-auto">
+                            <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                <TransitionChild
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 scale-95"
+                                    enterTo="opacity-100 scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 scale-100"
+                                    leaveTo="opacity-0 scale-95"
+                                >
+                                    <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                                        <p className="text-sm text-slate-400">Creation date: </p>
+                                        <p className="text-sm text-slate-400">Last update: </p>
+                                        <DialogTitle
+                                            as="h3"
+                                            className="font-black text-4xl text-slate-600 my-5"
+                                        >
+                                            {data.name}
+                                        </DialogTitle>
+                                        <p className="text-lg text-slate-500 mb-2">
+                                            {data.description}
+                                        </p>
+                                        <div className="my-5 space-y-3">
+                                            <label className="font-bold">Actual status:</label>
+                                        </div>
+                                    </DialogPanel>
+                                </TransitionChild>
+                            </div>
                         </div>
-                    </div>
-                </Dialog>
-            </Transition>
-        </>
-    );
+                    </Dialog>
+                </Transition>
+            </>
+        );
 }
