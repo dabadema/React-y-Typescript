4ba99849a9e555e31015104ca4139831010b5915
dabadema <dabadema7@hotmail.com>
2025-01-09 16:09:28 +0100

    Feat: npm run dev:api created to allow TC and PostMan clients

--- Uptask/backend_uptask/src/config/cors.ts
+++ Uptask/backend_uptask/src/config/cors.ts
@@ -2,7 +2,6 @@ import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        console.log(process.argv);
         const whitelist = [process.env.FRONTEND_URL];
 
         if (process.argv[2] === '--api') {
