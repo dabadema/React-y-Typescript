5bfd8ee3febeca9f86e404d173f144e011ec444d
dabadema <dabadema7@hotmail.com>
2025-01-09 23:39:40 +0100

    Feat: Hashing passwords when creating new user account

--- Uptask/backend_uptask/src/controllers/AuthController.ts
+++ Uptask/backend_uptask/src/controllers/AuthController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
 import User from '../models/User';
+import { hashPassword } from '../utils/auth';
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
         try {
             const user = new User(req.body);
+            const { password } = req.body;
+
+            // Hash password
+            user.password = await hashPassword(password);
+
             await user.save();
             res.send('User account created, please check your email for confirmation');
         } catch (error) {
