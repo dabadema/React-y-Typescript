3c3b5af930d4767d04096a5e70d15e49372626cb
dabadema <dabadema7@hotmail.com>
2025-01-09 23:22:58 +0100

    Feat: Creating AuthController and Route

---
+++ Uptask/backend_uptask/src/controllers/AuthController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+export class AuthController {
+    static createAccount = async (req: Request, res: Response) => {
+        res.send('Desde /api/auth');
+    };
+}
--- Uptask/backend_uptask/src/routes/authRoute.ts
+++ Uptask/backend_uptask/src/routes/authRoute.ts
@@ -1,9 +1,25 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { handleInputErrors } from '../middleware/validation';
+import { AuthController } from '../controllers/AuthController';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Desde /api/auth');
-});
+router.post(
+    '/create-account',
+    [
+        body('email').isEmail().withMessage('Invalid email'),
+        body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters'),
+        body('password_confirmation').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords are not matching');
+            }
+            return true;
+        }),
+        body('name').notEmpty().withMessage('Name is required'),
+    ],
+    handleInputErrors,
+    AuthController.createAccount
+);
 
 export default router;
