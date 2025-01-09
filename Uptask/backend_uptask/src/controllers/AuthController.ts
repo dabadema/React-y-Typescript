e4962667970d6c9f4078959e6f2f4cc9387f91ff
dabadema <dabadema7@hotmail.com>
2025-01-09 23:29:21 +0100

    Feat: Function for createAccount

--- Uptask/backend_uptask/src/controllers/AuthController.ts
+++ Uptask/backend_uptask/src/controllers/AuthController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
-        res.send('Desde /api/auth');
+        try {
+            const user = new User(req.body);
+            await user.save();
+            res.send('User account created, please check your email for confirmation');
+        } catch (error) {
+            res.status(500).json({ error: 'There was an error' });
+        }
     };
 }
