19f674310725b775fd4e0593426f5c56ff843d4d
dabadema <dabadema7@hotmail.com>
2025-01-09 15:49:24 +0100

    Fix: Cors adapted with "dev:api"

--- Uptask/backend_uptask/src/config/cors.ts
+++ Uptask/backend_uptask/src/config/cors.ts
@@ -2,12 +2,17 @@ import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        if (!origin || process.env.FRONTEND_URL === origin) {
+        console.log(process.argv);
+        const whitelist = [process.env.FRONTEND_URL];
+
+        if (process.argv[2] === '--api') {
+            whitelist.push(undefined); // Allow undefined origin as Thunderclient and Postman
+        }
+
+        if (whitelist.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
 };
