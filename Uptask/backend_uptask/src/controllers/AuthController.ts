c0f8b09709221ecf3dfc33c5619ec7ec1717e24e
dabadema <dabadema7@hotmail.com>
2025-01-10 11:15:32 +0100

    Feat: Preventing duplicate users

--- Uptask/backend_uptask/src/controllers/AuthController.ts
+++ Uptask/backend_uptask/src/controllers/AuthController.ts
@@ -5,8 +5,17 @@ import { hashPassword } from '../utils/auth';
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
         try {
+            const { password, email } = req.body;
+
+            /** Preventing duplicate accounts */
+            const userExists = await User.findOne({ email });
+            if (userExists) {
+                const error = new Error('User already exists');
+                res.status(409).json({ error: error.message });
+                return;
+            }
+
             const user = new User(req.body);
-            const { password } = req.body;
 
             // Hash password
             user.password = await hashPassword(password);
