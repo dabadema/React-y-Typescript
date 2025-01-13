8fb30f48deabcd6b4c7317b250356adea350dae5
dabadema <dabadema7@hotmail.com>
2025-01-13 18:56:16 +0100

    Feat: Email modified for navigating to /confirm-account

--- Uptask/backend_uptask/src/emails/AuthEmail.ts
+++ Uptask/backend_uptask/src/emails/AuthEmail.ts
@@ -14,7 +14,7 @@ export class AuthEmail {
             subject: 'UpTask - Confirm your email',
             text: `Please confirm your email by clicking here: ${user.token}`,
             html: `<p>Hi ${user.name}, your account in UpTask has been created, everything is about to be ready, just waiting for your confirmation! </p>
-            <p> Visit the following link to confirm your email: <a href="${process.env.FRONTEND_URL}/confirm/${user.token}">Confirm account</a></p>
+            <p> Visit the following link to confirm your email: <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirm account</a></p>
             <p> and introduce the code: <b>${user.token}</b> to complete the process.</p>
             <p> This token will expire in 10 minutes.</p>`,
         });
