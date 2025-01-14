8987795e930bbcc75826cc97fc12df5511f5160d
dabadema <dabadema7@hotmail.com>
2025-01-14 23:59:35 +0100

    Type: ForgotPasswordForm

--- Uptask/uptask_frontend/src/types/index.ts
+++ Uptask/uptask_frontend/src/types/index.ts
@@ -18,6 +18,7 @@ export type UserRegistrationForm = Pick<
     'name' | 'email' | 'password' | 'password_confirmation'
 >;
 export type RequestConfirmationCodeForm = Pick<Auth, 'email'>;
+export type ForgotPasswordForm = Pick<Auth, 'email'>;
 export type ConfirmToken = Pick<Auth, 'token'>;
 
 /** Tasks */
