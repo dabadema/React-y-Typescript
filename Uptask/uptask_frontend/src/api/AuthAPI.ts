43d9779932d82e6d785274080c085bd827e128ae
dabadema <dabadema7@hotmail.com>
2025-01-14 22:41:08 +0100

    Feat: new function for AuthenticateUser

--- Uptask/uptask_frontend/src/api/AuthAPI.ts
+++ Uptask/uptask_frontend/src/api/AuthAPI.ts
@@ -1,6 +1,11 @@
 import api from '@/lib/axios';
 import { isAxiosError } from 'axios';
-import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from '../types';
+import {
+    ConfirmToken,
+    RequestConfirmationCodeForm,
+    UserLoginForm,
+    UserRegistrationForm,
+} from '../types';
 
 export async function createAccount(formData: UserRegistrationForm) {
     try {
@@ -29,6 +34,18 @@ export async function confirmAccount(formData: ConfirmToken) {
 export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
     try {
         const url = '/auth/request-code';
+        const { data } = await api.post<string>(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function AuthenticateUser(formData: UserLoginForm) {
+    try {
+        const url = '/auth/login';
         const { data } = await api.post<string>(url, formData);
         return data;
     } catch (error) {
