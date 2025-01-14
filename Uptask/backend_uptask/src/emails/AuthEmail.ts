051cbad2f0468fa17de13b1808bb37b9f96ed55c
dabadema <dabadema7@hotmail.com>
2025-01-14 23:22:03 +0100

    Feat: New email for reset password

--- Uptask/backend_uptask/src/emails/AuthEmail.ts
+++ Uptask/backend_uptask/src/emails/AuthEmail.ts
@@ -21,4 +21,18 @@ export class AuthEmail {
 
         console.log('Email sent', info.messageId);
     };
+
+    static sendPasswordResetToken = async (user: IEmail) => {
+        const info = await transporter.sendMail({
+            from: 'UpTask <noreply@uptask.com>',
+            to: user.email,
+            subject: 'UpTask - Set a new password',
+            text: `UpTask - Set a new password`,
+            html: `<p>Hi ${user.name},</p> <p> You have requested to set a new password.</p> <p> Visit the following link: <a href="${process.env.FRONTEND_URL}/auth/forgot-password ">Set a new password</a>
+            and introduce the code: <b>${user.token}</b> to complete the process.</p>
+            <p> This token will expire in 10 minutes.</p>`,
+        });
+
+        console.log('Email sent', info.messageId);
+    };
 }
