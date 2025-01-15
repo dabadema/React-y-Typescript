9140298d3e050cf05ad596e95ba493a3f35f3c00
dabadema <dabadema7@hotmail.com>
2025-01-15 11:51:28 +0100

    Feat: NewPasswordToken component created and included into view conditionally with states

--- Uptask/uptask_frontend/src/components/auth/NewPasswordToken.tsx
+++ Uptask/uptask_frontend/src/components/auth/NewPasswordToken.tsx
@@ -1,16 +1,46 @@
+import { ConfirmToken } from '@/types/index';
 import { PinInput, PinInputField } from '@chakra-ui/pin-input';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { validateToken } from '@/api/AuthAPI';
+import { toast } from 'react-toastify';
 
-export default function NewPasswordToken() {
-    const handleChange = (token: string) => {};
-    const handleComplete = (token: string) => {};
+type NewPasswordTokenProps = {
+    token: ConfirmToken['token'];
+    setToken: React.Dispatch<React.SetStateAction<string>>;
+    setIsValidToken: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function NewPasswordToken({
+    token,
+    setToken,
+    setIsValidToken,
+}: NewPasswordTokenProps) {
+    const { mutate } = useMutation({
+        mutationFn: validateToken,
+        onError: (error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            toast.success(data);
+        },
+    });
+
+    const handleChange = (token: ConfirmToken['token']) => {
+        setToken(token);
+    };
+
+    const handleComplete = (token: ConfirmToken['token']) => {
+        mutate({ token });
+        setIsValidToken(true);
+    };
 
     return (
         <>
-            <form className="space-y-8 p-10 rounded-lg bg-white mt-10">
+            <form className="space-y-8 p-10 bg-white rounded-lg mt-10">
                 <label className="font-normal text-2xl text-center block">6 digits Code</label>
                 <div className="flex justify-center gap-5">
-                    <PinInput value={''} onChange={handleChange} onComplete={handleComplete}>
+                    <PinInput value={token} onChange={handleChange} onComplete={handleComplete}>
                         <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
                         <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
                         <PinInputField className="h-10 w-10 p-3 rounded-lg border-gray-300 border placeholder-white" />
--- Uptask/uptask_frontend/src/views/auth/NewPasswordView.tsx
+++ Uptask/uptask_frontend/src/views/auth/NewPasswordView.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import NewPasswordForm from '@/components/auth/NewPasswordForm';
+import NewPasswordToken from '@/components/auth/NewPasswordToken';
+import { ConfirmToken } from '@/types/index';
+import React, { useState } from 'react';
 
 export default function NewPasswordView() {
-    return <div>NewPasswordView</div>;
+    const [token, setToken] = useState<ConfirmToken['token']>('');
+    const [isValidToken, setIsValidToken] = useState(false);
+
+    return (
+        <>
+            <h1 className="text-5xl font-black text-white">Reset Password</h1>
+            <p className="text-2xl font-light text-white mt-5">
+                Introduce the code received {''}
+                <span className=" text-fuchsia-500 font-bold"> by email</span>
+            </p>
+
+            {!isValidToken ? (
+                <NewPasswordToken
+                    token={token}
+                    setToken={setToken}
+                    setIsValidToken={setIsValidToken}
+                />
+            ) : (
+                <NewPasswordForm />
+            )}
+        </>
+    );
 }
