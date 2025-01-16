d7c3120e698b3b56040c38666f2bd81bb7e7e72d
dabadema <dabadema7@hotmail.com>
2025-01-16 13:28:45 +0100

    Feat: decoding jwt and checking that exists in db

--- Uptask/backend_uptask/src/middleware/auth.ts
+++ Uptask/backend_uptask/src/middleware/auth.ts
@@ -1,15 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization;
 
     if (!bearer) {
-        const error = new Error('No autorizado');
+        const error = new Error('Not authorized');
         res.status(401).json({ error: error.message });
         return;
     }
     const token = bearer.split(' ')[1];
-    console.log(token);
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (typeof decoded === 'object' && decoded.id) {
+            const user = await User.findById(decoded.id);
+            console.log(user);
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Token not valid' });
+    }
 
     next();
 };
