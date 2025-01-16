cddacdc6e4277ffca82638057c52bfb1bec59b15
dabadema <dabadema7@hotmail.com>
2025-01-16 13:37:13 +0100

    Feat: Interface created for redefining Request interface with user?: IUser

--- Uptask/backend_uptask/src/middleware/auth.ts
+++ Uptask/backend_uptask/src/middleware/auth.ts
@@ -1,6 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../models/User';
+import User, { IUser } from '../models/User';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser;
+        }
+    }
+}
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization;
@@ -16,8 +24,13 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         if (typeof decoded === 'object' && decoded.id) {
-            const user = await User.findById(decoded.id);
-            console.log(user);
+            const user = await User.findById(decoded.id).select('_id name email');
+            if (user) {
+                // Writing over request object
+                req.user = user;
+            } else {
+                res.status(500).json({ error: 'Token not valid' });
+            }
         }
     } catch (error) {
         res.status(500).json({ error: 'Token not valid' });
