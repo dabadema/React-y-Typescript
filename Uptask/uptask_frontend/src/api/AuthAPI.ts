9e84068cdb12b248896f01a90257a3793e5c45bf
dabadema <dabadema7@hotmail.com>
2025-01-20 23:31:06 +0100

    Feat: API function (backend &frontend) to getUser data

--- Uptask/uptask_frontend/src/api/AuthAPI.ts
+++ Uptask/uptask_frontend/src/api/AuthAPI.ts
@@ -99,3 +99,15 @@ export async function updatePasswordWithToken({
         }
     }
 }
+
+export async function getUser() {
+    try {
+        const { data } = await api.get('/auth/user');
+        console.log(data);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
