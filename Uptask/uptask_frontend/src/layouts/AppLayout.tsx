e678f2651e322dd2c2712b7fafc6187233b69c76
dabadema <dabadema7@hotmail.com>
2025-01-21 15:14:52 +0100

    Feat: Showing name of the user logged in the application

--- Uptask/uptask_frontend/src/layouts/AppLayout.tsx
+++ Uptask/uptask_frontend/src/layouts/AppLayout.tsx
@@ -25,7 +25,7 @@ export default function AppLayout() {
                                 <Logo />
                             </Link>
                         </div>
-                        <NavMenu />
+                        <NavMenu name={data.name} />
                     </div>
                 </header>
 
