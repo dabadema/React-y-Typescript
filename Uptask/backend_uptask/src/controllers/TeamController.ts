59d91c6b8f3a77f02e290b00b4e971bc3250fa10
dabadema <dabadema7@hotmail.com>
2025-01-21 19:44:19 +0100

    Feat: Methods for adding and removing an user from a project

--- Uptask/backend_uptask/src/controllers/TeamController.ts
+++ Uptask/backend_uptask/src/controllers/TeamController.ts
@@ -14,4 +14,45 @@ export class TeamMemberController {
         }
         res.json(user);
     };
+
+    static addMemberById = async (req: Request, res: Response) => {
+        const { _id } = req.body;
+
+        // Find user
+        const user = await User.findById({ _id }).select('_id');
+        if (!user) {
+            const error = new Error('User not found');
+            res.status(409).json({ error: error.message });
+            return;
+        }
+
+        // Adding user to the team
+
+        if (req.project.team.some((team) => team.toString() === user.id.toString())) {
+            const error = new Error('User already exists in the project');
+            res.status(404).json({ error: error.message });
+            return;
+        }
+
+        req.project.team.push(user.id);
+        await req.project.save();
+
+        res.send('User added successfully');
+    };
+
+    static removeMemberById = async (req: Request, res: Response) => {
+        const { _id } = req.body;
+
+        if (!req.project.team.some((team) => team.toString() === _id.toString())) {
+            const error = new Error('User does not exist in the project');
+            res.status(404).json({ error: error.message });
+            return;
+        }
+
+        req.project.team = req.project.team.filter((teamMember) => teamMember.toString() !== _id);
+
+        await req.project.save();
+
+        res.send('User removed successfully');
+    };
 }
