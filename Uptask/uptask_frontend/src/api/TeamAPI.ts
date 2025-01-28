c1a9e621f32d5643a66938157a505c7a6719e759
dabadema <dabadema7@hotmail.com>
2025-01-28 23:50:15 +0100

    Feat: API function modified to destructure {data} and correctly naming "_id" to be received as defined in backend

--- Uptask/uptask_frontend/src/api/TeamAPI.ts
+++ Uptask/uptask_frontend/src/api/TeamAPI.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from 'axios';
 import api from '@/lib/axios';
-import { Project, TeamMemberForm } from '../types';
+import { Project, TeamMember, TeamMemberForm } from '../types';
 
 export async function findUserByEmail({
     projectId,
@@ -11,7 +11,25 @@ export async function findUserByEmail({
 }) {
     try {
         const url = `/projects/${projectId}/team/find`;
-        const data = await api.post(url, formData);
+        const { data } = await api.post(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function addUserToProject({
+    projectId,
+    _id,
+}: {
+    projectId: Project['_id'];
+    _id: TeamMember['_id'];
+}) {
+    try {
+        const url = `/projects/${projectId}/team`;
+        const { data } = await api.post(url, { _id });
         return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
