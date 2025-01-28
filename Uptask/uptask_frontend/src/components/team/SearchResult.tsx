3849144ac200714c8728859fd8b94b1331a2bb24
dabadema <dabadema7@hotmail.com>
2025-01-28 23:51:34 +0100

    Feat: Arguments and mutation prepared to addNewUserToProject

--- Uptask/uptask_frontend/src/components/team/SearchResult.tsx
+++ Uptask/uptask_frontend/src/components/team/SearchResult.tsx
@@ -1,18 +1,46 @@
+import { addUserToProject } from '@/api/TeamAPI';
 import { TeamMember } from '@/types/index';
+import { useMutation } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 type SearchResultProps = {
     user: TeamMember;
 };
 
 export default function SearchResult({ user }: SearchResultProps) {
-    console.log(user);
+    const params = useParams();
+    const projectId = params.projectId!;
+
+    const { mutate } = useMutation({
+        mutationFn: addUserToProject,
+        onError: (error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            toast.success(data);
+        },
+    });
+
+    const handleAddUserToProject = () => {
+        const data = {
+            projectId,
+            _id: user._id,
+        };
+        console.log(data);
+        mutate(data);
+    };
+
     return (
         <>
             <p className="mt-10 text-center font-bold"> Result: </p>
             <div className="flex justify-between items-center">
                 <p>{user.name}</p>
-                <button className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer">
-                    Add to the project
+                <button
+                    className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer"
+                    onClick={handleAddUserToProject}
+                >
+                    Add to project
                 </button>
             </div>
         </>
