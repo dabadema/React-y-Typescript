9eb05161280d1d7ebba2563d2b784d741ce2b33b
dabadema <dabadema7@hotmail.com>
2025-01-30 23:44:47 +0100

    Feat: API function removeUserFromProject created

--- Uptask/uptask_frontend/src/api/TeamAPI.ts
+++ Uptask/uptask_frontend/src/api/TeamAPI.ts
@@ -29,7 +29,7 @@ export async function addUserToProject({
 }) {
     try {
         const url = `/projects/${projectId}/team`;
-        const { data } = await api.post(url, { _id });
+        const { data } = await api.post<string>(url, { _id });
         return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
@@ -47,6 +47,24 @@ export async function getProjectTeam({ projectId }: { projectId: Project['_id'] 
         if (response.success) {
             return response.data;
         }
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function removeUserFromProject({
+    projectId,
+    userId,
+}: {
+    projectId: Project['_id'];
+    userId: TeamMember['_id'];
+}) {
+    try {
+        const url = `/projects/${projectId}/team/${userId}`;
+        const { data } = await api.delete<string>(url);
+        return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
