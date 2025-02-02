5a6f76066ff0e397ba8c4dc1eee0f554cbfe96f4
dabadema <dabadema7@hotmail.com>
2025-02-02 23:46:12 +0100

    Feat: Adding a badge for identifying either a manager or partner into the dashboardView

--- Uptask/uptask_frontend/src/views/DashboardView.tsx
+++ Uptask/uptask_frontend/src/views/DashboardView.tsx
@@ -55,6 +55,18 @@ export default function DashboardView() {
                             >
                                 <div className="flex min-w-0 gap-x-4">
                                     <div className="min-w-0 flex-auto space-y-2">
+                                        <div className="mb-2">
+                                            {project.manager === user._id ? (
+                                                <p className="font-bold text-xs bg-indigo-50 text-indigo-500 border-2 border-indigo-500 rounded-lg inline-block py-1 px-5">
+                                                    Manager
+                                                </p>
+                                            ) : (
+                                                <p className="font-bold text-xs bg-green-50 text-green-500 border-2 border-green-500 rounded-lg inline-block py-1 px-5">
+                                                    Partner
+                                                </p>
+                                            )}
+                                        </div>
+
                                         <Link
                                             to={`/projects/${project._id}`}
                                             className="text-gray-600 cursor-pointer hover:underline text-3xl font-bold"
