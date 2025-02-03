f085bc069d0882c73bc4ef11056bfcc2ae1b6da3
dabadema <dabadema7@hotmail.com>
2025-02-03 21:07:11 +0100

    Refactor: isManager function applied to DashboardView

--- Uptask/uptask_frontend/src/views/DashboardView.tsx
+++ Uptask/uptask_frontend/src/views/DashboardView.tsx
@@ -6,6 +6,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteProject, getProjects } from '@/api/ProjectAPI';
 import { toast } from 'react-toastify';
 import { useAuth } from '@/hooks/useAuth';
+import isManager from '@/utils/policies';
 
 export default function DashboardView() {
     const { data: user, isLoading: authLoading } = useAuth();
@@ -56,7 +57,7 @@ export default function DashboardView() {
                                 <div className="flex min-w-0 gap-x-4 ">
                                     <div className="min-w-0 flex-auto space-y-2">
                                         <div className="mb-2">
-                                            {project.manager === user._id ? (
+                                            {isManager(project.manager, user._id) ? (
                                                 <p className="font-bold text-xs bg-indigo-50 text-indigo-500 border-2 border-indigo-500 rounded-lg inline-block py-1 px-5">
                                                     Manager
                                                 </p>
