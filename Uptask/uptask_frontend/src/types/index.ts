422ccc1140a783ed62a2418f555d15574a125877
dabadema <dabadema7@hotmail.com>
2025-02-05 23:50:27 +0100

    Feat: taskSchema updated to be an object

--- Uptask/uptask_frontend/src/types/index.ts
+++ Uptask/uptask_frontend/src/types/index.ts
@@ -52,7 +52,12 @@ export const taskSchema = z.object({
     description: z.string(),
     project: z.string(),
     status: taskStatusSchema,
-    completedBy: userSchema.or(z.null()),
+    completedBy: z.array(
+        z.object({
+            user: userSchema,
+            status: taskStatusSchema,
+        })
+    ),
     createdAt: z.string(),
     updatedAt: z.string(),
 });
