1b95c69f0e27b00c42316306643d131b61c504dd
dabadema <dabadema7@hotmail.com>
2025-02-10 18:02:35 +0100

    Feat: features from react-hook-form implemented for validation, register and handleSubmit

--- Uptask/uptask_frontend/src/components/notes/AddNoteForm.tsx
+++ Uptask/uptask_frontend/src/components/notes/AddNoteForm.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { NoteFormData } from '@/types/index';
+import { useForm } from 'react-hook-form';
+import ErrorMessage from '../ErrorMessage';
 
 export default function AddNoteForm() {
+    const initialValues: NoteFormData = {
+        content: '',
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ defaultValues: initialValues });
+
+    const handleAddNote = (formData: NoteFormData) => {
+        console.log(formData);
+    };
+
     return (
         <>
-            <form onSubmit={() => {}} className="space-y-3" noValidate>
+            <form onSubmit={handleSubmit(handleAddNote)} className="space-y-3" noValidate>
                 <div className="flex flex-col gap-2">
                     <label className="font-bold" htmlFor="content">
                         {' '}
@@ -14,7 +30,11 @@ export default function AddNoteForm() {
                         type="text"
                         placeholder="Note content"
                         className="w-full p-3 border border-gray-300"
+                        {...register('content', {
+                            required: 'Note content is mandatory',
+                        })}
                     />
+                    {errors.content && <ErrorMessage>{errors.content.message}</ErrorMessage>}
                 </div>
                 <input
                     type="submit"
