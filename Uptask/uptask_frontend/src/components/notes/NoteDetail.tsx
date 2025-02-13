1f635da31bc9de1074025f1810d0c7ab5367f167
dabadema <dabadema7@hotmail.com>
2025-02-13 10:48:59 +0100

    Feat: Allowing to remove the note when the user matches the creator

--- Uptask/uptask_frontend/src/components/notes/NoteDetail.tsx
+++ Uptask/uptask_frontend/src/components/notes/NoteDetail.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from '@/hooks/useAuth';
 import { Note } from '@/types/index';
 import { formatDate } from '@/utils/utils';
 
@@ -6,12 +7,18 @@ type NoteDetailProps = {
 };
 
 export default function NoteDetail({ note }: NoteDetailProps) {
-    return (
-        <div className="p-3 flex-col justify-between items-center ">
-            <p>
-                {note.content} por: <span className="font-bold"> {note.createdBy.name} </span>
-            </p>
-            <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
-        </div>
-    );
+    const { data, isLoading } = useAuth();
+
+    if (isLoading) return 'Cargando...';
+    if (data)
+        return (
+            <div className="p-3 flex-col justify-between items-center ">
+                <p>
+                    {note.content} por: <span className="font-bold"> {note.createdBy.name} </span>
+                </p>
+                <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
+
+                {data._id === note.createdBy._id && <button>Delete</button>}
+            </div>
+        );
 }
