927105367f699b14988ffb4602364653a8df9de4
dabadema <dabadema7@hotmail.com>
2025-02-13 09:53:46 +0100

    Fix: populate the right path and selects for ZOD validation

--- Uptask/backend_uptask/src/controllers/TaskController.ts
+++ Uptask/backend_uptask/src/controllers/TaskController.ts
@@ -34,7 +34,6 @@ export class TaskController {
                 })
                 .populate({
                     path: 'notes',
-                    select: 'id content',
                     populate: { path: 'createdBy', select: 'id name email' },
                 });
 
