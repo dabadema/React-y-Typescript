ab400f3506128278d978749f2cfed405fb6f8672
dabadema <dabadema7@hotmail.com>
2025-02-13 11:00:52 +0100

    Refactor: Using useMemo instead of direct conditional within the return

--- Uptask/uptask_frontend/src/components/notes/NoteDetail.tsx
+++ Uptask/uptask_frontend/src/components/notes/NoteDetail.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from '@/hooks/useAuth';
 import { Note } from '@/types/index';
 import { formatDate } from '@/utils/utils';
+import { useMemo } from 'react';
 
 type NoteDetailProps = {
     note: Note;
@@ -9,16 +10,22 @@ type NoteDetailProps = {
 export default function NoteDetail({ note }: NoteDetailProps) {
     const { data, isLoading } = useAuth();
 
+    const canDelete = useMemo(() => data?._id === note.createdBy._id, [data]);
+
     if (isLoading) return 'Cargando...';
     if (data)
         return (
-            <div className="p-3 flex-col justify-between items-center ">
-                <p>
-                    {note.content} por: <span className="font-bold"> {note.createdBy.name} </span>
-                </p>
-                <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
-
-                {data._id === note.createdBy._id && <button>Delete</button>}
-            </div>
+            <>
+                <div className="p-3 flex justify-between items-center ">
+                    <div>
+                        <p>
+                            {note.content} por:{' '}
+                            <span className="font-bold"> {note.createdBy.name} </span>
+                        </p>
+                        <p className="text-xs text-slate-500 mt-2">{formatDate(note.createdAt)}</p>
+                    </div>
+                    {canDelete && <button className=" hover:text-red-500">Delete</button>}
+                </div>
+            </>
         );
 }
