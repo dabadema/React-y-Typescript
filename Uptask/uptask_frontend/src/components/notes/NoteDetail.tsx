950e5b5d18f2effed63b53b26305e7c71fa97688
dabadema <dabadema7@hotmail.com>
2025-02-13 10:06:34 +0100

    Feat: Showing content, creator and date of the note

--- Uptask/uptask_frontend/src/components/notes/NoteDetail.tsx
+++ Uptask/uptask_frontend/src/components/notes/NoteDetail.tsx
@@ -1,3 +1,17 @@
-export default function NoteDetail() {
-    return <div>NoteDetail</div>;
+import { Note } from '@/types/index';
+import { formatDate } from '@/utils/utils';
+
+type NoteDetailProps = {
+    note: Note;
+};
+
+export default function NoteDetail({ note }: NoteDetailProps) {
+    return (
+        <div className="p-3 flex-col justify-between items-center ">
+            <p>
+                {note.content} por: <span className="font-bold"> {note.createdBy.name} </span>
+            </p>
+            <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
+        </div>
+    );
 }
