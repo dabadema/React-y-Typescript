96f77863957e4b5451952663bf850840488b6edd
dabadema <dabadema7@hotmail.com>
2025-02-20 23:48:36 +0100

    Feat: API to interact for changing password

--- Uptask/uptask_frontend/src/api/ProfileAPI.ts
+++ Uptask/uptask_frontend/src/api/ProfileAPI.ts
@@ -1,11 +1,23 @@
 import { isAxiosError } from 'axios';
-import { UserProfileForm } from '../types';
+import { UpdateCurrentPasswordForm, UserProfileForm } from '../types';
 import api from '@/lib/axios';
 
 export async function updateProfile(formData: UserProfileForm) {
     try {
         const url = `/auth/profile`;
         const { data } = await api.put<string>(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function changePassword(formData: UpdateCurrentPasswordForm) {
+    try {
+        const url = `/auth/update-password`;
+        const { data } = await api.post<string>(url, formData);
         return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
