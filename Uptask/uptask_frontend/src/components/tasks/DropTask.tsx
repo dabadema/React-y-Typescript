001ffc2d6eb3dc18738e330f98cb735dfcecf292
dabadema <dabadema7@hotmail.com>
2025-02-21 14:02:42 +0100

    Feat: DropTask created draft version

---
+++ Uptask/uptask_frontend/src/components/tasks/DropTask.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DropTask() {
+    return (
+        <div className="text-xs font-semibold border border-dashed border-slate-500 uppercase p-2 mt-5 grid place-content-center text-slate-500">
+            Drop the Task here
+        </div>
+    );
+}
