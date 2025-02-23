1fd3d502a46335c8cd8cadb874a07f48f1893407
dabadema <dabadema7@hotmail.com>
2025-02-23 23:34:15 +0100

    Feat: Adding visual feedback when isOver droppable component

--- Uptask/uptask_frontend/src/components/tasks/DropTask.tsx
+++ Uptask/uptask_frontend/src/components/tasks/DropTask.tsx
@@ -1,4 +1,5 @@
 import { useDroppable } from '@dnd-kit/core';
+import { styleText } from 'util';
 
 type DropTaskProps = {
     status: string;
@@ -9,8 +10,13 @@ export default function DropTask({ status }: DropTaskProps) {
         id: status,
     });
 
+    const style = {
+        opacity: isOver ? 0.4 : undefined,
+    };
+
     return (
         <div
+            style={style}
             ref={setNodeRef}
             className="text-xs font-semibold border border-dashed border-slate-500 uppercase p-2 mt-5 grid place-content-center text-slate-500"
         >
