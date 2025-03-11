12f5c77a56bd82c003a2b3a9bcd118e1e23e008e
dabadema <dabadema7@hotmail.com>
2025-03-11 01:43:57 +0100

    Feat: last attempt

--- Uptask/uptask_frontend/src/lib/axios.ts
+++ Uptask/uptask_frontend/src/lib/axios.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
+console.log('api:', api);
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('AUTH_TOKEN');
